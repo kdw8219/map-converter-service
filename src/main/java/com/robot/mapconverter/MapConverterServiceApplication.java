24b7ec2c795cb9b16778d5548eeefe8f68f196e8
kdw8219 <kdw8219@gmail.com>
2026-01-30 17:20:16 +0900

    Feature  - first commit

---
+++ src/main/java/com/robot/mapconverter/MapConverterServiceApplication.java
@@ -0,0 +1,11 @@
+package com.robot.mapconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapConverterServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapConverterServiceApplication.class, args);
+    }
+}
